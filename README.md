c93e20e34474a9b15f656b737c3f39850218f54e
Joris Mangel <joris.mangel@gmail.com>
2017-04-13 12:37:32 +1200

    Downlink message first. Display more information. Readme updated.

--- README.md
+++ README.md
@@ -2,12 +2,17 @@
 
 ## Synopsis
 
+Sample Code for the Pycom SiPy.
+A way to get started with Sigfox, the devkit SiPy boasts a full suite of features and accessories to empower anyone to set up an IoT solution, even with very little hardware experience.
+
 Thinxtra is a Sigfox operator based on Australia/NZ/HK.
 The aim of this project is to perform some tests on Pycom SiPy using Thinxtra's Sigfox network.
 
 ## Code Example
 
-Send a Sigfox message:
+Send a Sigfox message
+
+-UPLINK only:
 ```python
 # init Sigfox for RCZ4 (Austrlia/NZ)
 sigfox = Sigfox(mode=Sigfox.SIGFOX, rcz=Sigfox.RCZ4)
@@ -22,9 +27,34 @@ s = socket.socket(socket.AF_SIGFOX, socket.SOCK_RAW)
 # make the socket blocking
 s.setblocking(True)
 
-#configure it as uplink
+# configure it as uplink
 s.setsockopt(socket.SOL_SIGFOX, socket.SO_RX, False)
 
-#send bytes
+# send hello message
 s.send("Hello world")
+```
+
+-DOWNLINK requested:
+```python
+# init Sigfox for RCZ4 (Austrlia/NZ)
+sigfox = Sigfox(mode=Sigfox.SIGFOX, rcz=Sigfox.RCZ4)
+
+# print device ID and PAC
+print({binascii.hexlify(sigfox.id())})
+print({binascii.hexlify(sigfox.pac())})
+
+# create a Sigfox socket
+s = socket.socket(socket.AF_SIGFOX, socket.SOCK_RAW)
+
+# make the socket blocking
+s.setblocking(True)
+
+# configure it as downlink specified by 'True'
+s.setsockopt(socket.SOL_SIGFOX, socket.SO_RX, True)
+
+# send hello message
+s.send("Hello world")
+
+# await downlink message
+print("Message received: ",  s.recv(32))
 ```
--- Test/main.py
+++ Test/main.py
@@ -1,17 +1,41 @@
-### Thinxtra Pycom SiPy test ###
+###
+###  Thinxtra Pycom SiPy test  - Test application for SiPy with Expansion board
+###  Version 1.0
+###  Created by Joris Mangel, Thinxtra Pty.
+###  April 13, 2017.
+  
+###  Released into the public domain.
+###
+
 from network import Sigfox
 from machine import Pin
 import socket
 import pycom
 import time
-#import binascii
-#import os
+import binascii
+import os
+
+
+### INIT phase ###
+print("Thinxtra Pycom SiPy test ")
+
+# Print device firmware
+print("Device firmware: ",  os.uname().release)
 
 # Disable the heartbeat led
 pycom.heartbeat(False)
  
 # initialize GP17 in gpio mode and make it an input with the pull-up enabled
 button = Pin("G17", Pin.IN, pull=Pin.PULL_UP)
+
+# variables
+i=0
+j=0
+
+### END INIT ###
+
+
+### WAIT BUTTON CLICK ###
 
 # Wait until button click - LED blinking
 while(button.value()):
@@ -21,54 +45,65 @@ while(button.value()):
     time.sleep(0.2)
     pycom.rgbled(0x1EAFBB) 
     time.sleep(0.2)
+pycom.rgbled(0)  # turn off the LED  
 
-# turn off the LED  
-pycom.rgbled(0)  
+### END WAIT BUTTON ###
+
+
+### SIGFOX COMMUNICATION ###
     
 # init Sigfox for RCZ4 (Austrlia/NZ)
 sigfox = Sigfox(mode=Sigfox.SIGFOX, rcz=Sigfox.RCZ4)
-#print({binascii.hexlify(sigfox.id())})
-#print({binascii.hexlify(sigfox.pac())})
 
-i=0
-j=0
+# Print device ID and PAC
+print("Device ID: ",  {binascii.hexlify(sigfox.id())})
+print("Device PAC ",  {binascii.hexlify(sigfox.pac())})
 
 try:
     # create a Sigfox socket
     s = socket.socket(socket.AF_SIGFOX, socket.SOCK_RAW)
-    #print("socket created!")
 
     # make the socket blocking
     s.setblocking(True)
+    
+    
+    # FIRST MESSAGE REQUESTS DOWNLINK
+    print("\nDOWNLINK MESSAGE...")
+    
+    # configure it as DOWNLINK specified by 'True'
+    s.setsockopt(socket.SOL_SIGFOX, socket.SO_RX, True)
+    
+    # send hello message
+    print("Payload sent: hello")
+    pycom.rgbled(0x007f00) # LED green
+    input=s.send("hello")
+    pycom.rgbled(0) # turn off the LED        
+    print("Nb bytes sent: ", input,  "\n") #number of bytes sent
+        
+    # await DOWNLINK message
+    print("Message received: ",  s.recv(32))
+    
+    
+    # OTHER MESSAGES ALL UPLINK ONLY
+    print("\nUPLINK MESSAGES...")
 
     #configure it as uplink only
     s.setsockopt(socket.SOL_SIGFOX, socket.SO_RX, False)
-
-    # configure it as DOWNLINK specified by 'True'
-    #s.setsockopt(socket.SOL_SIGFOX, socket.SO_RX, True)
 
     #send some bytes & blink LED  
     while(True): 
         time.sleep(5)
         
         #send bytes
-        pycom.rgbled(0x007f00) # green
-        print("payload: i=", i, "j=", j)
+        print("Payload sent: i=", i, "j=", j)
+        pycom.rgbled(0x007f00) # LED green
         input=s.send(bytes([i, j]))
-        print("nb bytes sent: ", input,  "\n") #number of bytes sent
-        i=(i+1)%16
-        j=j+1
         pycom.rgbled(0) # turn off the LED        
+        print("Nb bytes sent: ", input,  "\n") #number of bytes sent
+        i=(i+1)%16
+        j=(j+1)%256
         
-        time.sleep(5) 
-    
-    # await DOWNLINK message
-    #print("await DOWNLINK message...")
-    #input = s.recv(32)
-    #print(output)
-    #print("... Downlink response ... ")
-    #print(binascii.hexlify(input))
-    #print("DOWNLINK message should have been recvd")
+        time.sleep(5)
 
 except Exception as e:
     pycom.rgbled(0x7f0000) # red
@@ -77,3 +112,5 @@ except Exception as e:
 finally:
     # close socket
     s.close()
+    
+### END SIGFOX COMMUNICATION ###
